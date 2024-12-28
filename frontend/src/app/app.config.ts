1c569a4fb762d17120f61e3fe79322f632fc0d36
hansi <hansi@hansi.de>
2024-12-28 10:37:16 +0100

    update to ngV18

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,7 @@ import {ApplicationConfig} from '@angular/core';
 import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations';
-import {provideHttpClient} from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import {MessageService} from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
