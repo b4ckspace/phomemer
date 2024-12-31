8f9f96e8e6ad58ddccfd8bcde8041c3b3e59a8e3
hansi <hansi@hansi.de>
2024-12-31 14:49:54 +0100

    test husky

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    { rules: { '@typescript-eslint/no-explicit-any': 'off', 'no-console': 'warn' } },
+    { ignores: ['**/node_modules/'] },
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+];
